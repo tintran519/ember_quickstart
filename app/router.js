8a78cf883c75b8cb6b17bb4322f7d8d6a80d9730
Tin Tran <tin.tran0519@gmail.com>
2017-09-03 22:35:04 -0700

    added routes

--- app/router.js
+++ app/router.js
@@ -8,6 +8,9 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('scientists');
+  this.route('about');
+  this.route('contact');
+  this.route('rentals');
 });
 
 export default Router;
